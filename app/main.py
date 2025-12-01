dc44a89de8750fa5901ec483c4cdce1a93eedac8
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-12-01 13:15:03 +0530

    feat: add /health endpoint fo service readiness

--- app/main.py
+++ app/main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message":"FastAPI on KIND is running!!"}
+    return {"message":"FastAPI on KIND is running!!!"}
+
+@app.get("/health",tags=['Health'])
+def health_check():
+    return {"status":"ok"}
