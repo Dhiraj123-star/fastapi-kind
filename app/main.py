4fcb2a00771c1aa96c176f6ff249e026e7b3565f
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-12-02 13:02:33 +0530

    Add ConfigMap-based environment management and update deployment

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
@@ -9,3 +10,11 @@ def read_root():
 @app.get("/health",tags=['Health'])
 def health_check():
     return {"status":"ok"}
+
+@app.get("/info")
+def info():
+    return {
+        "app_env": os.getenv("APP_ENV"),
+        "app_name": os.getenv("APP_NAME"),
+        "version": os.getenv("APP_VERSION"),
+    }
